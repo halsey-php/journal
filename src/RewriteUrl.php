b3e4bb8dc69bfe728ca18341d05655f599917500
Baptiste Langlade <baptouuuu@gmail.com>
2021-02-27 12:50:13 +0100

    only rewrite paths

--- src/RewriteUrl.php
+++ src/RewriteUrl.php
@@ -3,21 +3,34 @@ declare(strict_types = 1);
 
 namespace Halsey\Journal;
 
-use Innmind\Url\Url;
+use Innmind\Url\{
+    Url,
+    Authority,
+    Path,
+};
 use Innmind\Immutable\Str;
 
 final class RewriteUrl
 {
     public function __invoke(Url $url): Url
     {
-        $string = Str::of($url->toString());
-
-        if ($string->toLower()->endsWith('readme.md')) {
-            return Url::of($string->dropEnd(9)->append('index.html')->toString());
+        if (!$url->authority()->equals(Authority::none())) {
+            // then it must be an external link
+            return $url;
         }
 
-        if ($string->takeEnd(3)->toLower()->equals(Str::of('.md'))) {
-            return Url::of($string->dropEnd(3)->append('.html')->toString());
+        $path = Str::of($url->path()->toString());
+
+        if ($path->toLower()->endsWith('readme.md')) {
+            return $url->withPath(
+                Path::of($path->dropEnd(9)->append('index.html')->toString()),
+            );
+        }
+
+        if ($path->takeEnd(3)->toLower()->equals(Str::of('.md'))) {
+            return $url->withPath(
+                Path::of($path->dropEnd(3)->append('.html')->toString()),
+            );
         }
 
         return $url;
