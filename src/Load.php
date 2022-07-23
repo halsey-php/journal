c1e3fcd8db8e688321a2d6d030496f3de9a25171
Baptiste Langlade <baptouuuu@gmail.com>
2022-07-23 11:34:14 +0200

    use the filesystem abstraction to require the user config

--- src/Load.php
+++ src/Load.php
@@ -3,17 +3,31 @@ declare(strict_types = 1);
 
 namespace Halsey\Journal;
 
+use Innmind\OperatingSystem\Filesystem;
 use Innmind\Url\Path;
 
 final class Load
 {
+    private Filesystem $filesystem;
+
+    public function __construct(Filesystem $filesystem)
+    {
+        $this->filesystem = $filesystem;
+    }
+
     public function __invoke(Path $workingDirectory): Config
     {
         /**
-         * @psalm-suppress UnresolvableInclude
+         * @psalm-suppress MixedArgumentTypeCoercion
          * @var callable(Config): Config
          */
-        $configure = require $workingDirectory->resolve(Path::of('.journal'))->toString();
+        $configure = $this
+            ->filesystem
+            ->require($workingDirectory->resolve(Path::of('.journal')))
+            ->match(
+                static fn(callable $configure) => $configure,
+                static fn() => static fn(Config $config) => $config,
+            );
 
         return $configure(Config::of($workingDirectory));
     }
